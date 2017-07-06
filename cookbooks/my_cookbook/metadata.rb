4dca7d84edd340d7e8c94ad06f313d30a03e8edf
RavindraSinghKhichi <ravindrasinghkhichi@gmail.com>
2017-07-06 12:17:19 +0530

    9

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -7,14 +7,10 @@ long_description 'Installs/Configures my_cookbook'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/my_cookbook/issues'
+supports 'ubuntu'
 
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/my_cookbook'
+#if our cookbook supports multiple platforms 
+
+%w(debian ubuntu redhat centos fedora scientific amazon oracle).each do |os|
+  supports  os 
+end 
